ed4a1d54c299a3fb178c2ca2ebc73a89ce4ba1e6
ProsperousJoy <rizkysaputraamr006@gmail.com>
2024-12-18 11:51:48 +0700

    added onchange to textinput

--- src/components/inputs/InputText/InputText.jsx
+++ src/components/inputs/InputText/InputText.jsx
@@ -1,11 +1,13 @@
 import './InputText.scss'
 
-export default ({width, placeholder}) => {
+export default ({width, placeholder, onChange}) => {
+
     return(
         <input
       className="InputText"
       style={{ width: `${width}px` }}
       type="text"
+      onChange={onChange}
       placeholder={placeholder}
     />
     )
